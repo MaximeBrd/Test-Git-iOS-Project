b8e0dff4f8067c97635bb96a1f60dd85baf4c7a2
Maxime Borreda <MaximeBrd@Maximes-MacBook-Pro.local>
2014-08-22 10:53:29 -0700

    added a test class

---
+++ Git Test/MBTest.h
@@ -0,0 +1,13 @@
+//
+//  MBTest.h
+//  Git Test
+//
+//  Created by Maxime Borreda on 8/22/14.
+//  Copyright (c) 2014 Maxime Borreda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBTest : NSObject
+
+@end
---
+++ Git Test/MBTest.m
@@ -0,0 +1,13 @@
+//
+//  MBTest.m
+//  Git Test
+//
+//  Created by Maxime Borreda on 8/22/14.
+//  Copyright (c) 2014 Maxime Borreda. All rights reserved.
+//
+
+#import "MBTest.h"
+
+@implementation MBTest
+
+@end
